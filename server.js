292b3373b2734a2106db3f7d9a60fc9e4300f517
Penny <radiogaga66@gmail.com>
2019-03-23 21:13:39 -0500

    created server.js, orm, schema, seeds, handlebars

---
+++ server.js
@@ -0,0 +1,31 @@
+//Requirements
+var express = require("express");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
+
+//variable to run express
+var app = express();
+
+//set up port to make Heroku-friendly
+var PORT = process.env.PORT || 3000;
+
+//static route connection
+app.use(express.static("public"));
+
+//connecting body-parser elements (url and json)
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//setting up handlbars
+app.engine("handlebars", exphbs({  defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//routes
+var routes = require("./controllers/burgers-controller.js");
+// app.use(routes);
+
+//commands the port to listen
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost: " + PORT)
+});
+
