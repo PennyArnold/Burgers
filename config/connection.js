7e807b447cdce57f12b83e92e593fe31cc38f1c3
Penny <radiogaga66@gmail.com>
2019-04-01 20:52:24 -0500

    updated connection.js so that the Heroku connection works properly

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,28 @@
 //Requirement MySql
 var mysql = require("mysql");
+var connection;
 
 //creating connection to the database
-connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+connection = mysql.createConnection(process.env.JAWSBD_URL);
+} else {
+    connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "John2012",
     database: "burgers_db"
 });
-connection.connect(function(err) {
+};
+
+
+/* connection.connect(function(err) {
     if (err) {
         console.error("error connecting " + err.stack);
         return;
     }
-    console.log("connected as id " + connection.threadId);
-});
+    console.log("connected as id " + connection.threadId); */
+// });
 //export the module
+
+connection.connect();
 module.exports = connection;
