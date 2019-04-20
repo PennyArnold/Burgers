7ee20c7a712ebfdadf76513ac896de4314e551d7
Penny <radiogaga66@gmail.com>
2019-04-20 12:39:23 -0500

    updated db connection changes

--- config/connection.js
+++ config/connection.js
@@ -3,16 +3,17 @@ var mysql = require("mysql");
 var connection;
 
 //creating connection to the database
-if (process.env.JAWSDB_URL) {
+/* if (process.env.JAWSDB_URL) {
 connection = mysql.createConnection(process.env.JAWSBD_URL);
-} else {
+} else { */
     connection = mysql.createConnection({
     host: "s3lkt7lynu0uthj8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    port: 3306,
     user: "hqrbveyvjpv0nhzm",
     password: "bhyph9znfw6gpgqc",
-    database: "burgers_db"
+    database: "fu8jsmurk4pig4g1"
 });
-};
+/* }; */
 
 
 /* connection.connect(function(err) {
