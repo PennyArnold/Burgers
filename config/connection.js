df2283b0424ba5f9f76991d0704ee6f878058a23
Penny <radiogaga66@gmail.com>
2019-04-20 13:12:44 -0500

    connection.js update trying to find out why this isn't deploying correctly

--- config/connection.js
+++ config/connection.js
@@ -3,9 +3,9 @@ var mysql = require("mysql");
 var connection;
 
 //creating connection to the database
-if (process.env.JAWSDB_URL) {
+/* if (process.env.JAWSDB_URL) {
 connection = mysql.createConnection(process.env.JAWSBD_URL);
-} else {
+} else { */
     connection = mysql.createConnection({
     host: "s3lkt7lynu0uthj8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
@@ -13,16 +13,16 @@ connection = mysql.createConnection(process.env.JAWSBD_URL);
     password: "bhyph9znfw6gpgqc",
     database: "fu8jsmurk4pig4g1"
 });
- }; 
+//  }; 
 
 
-connection.connect(function(err) {
+/* connection.connect(function(err) {
     if (err) {
         console.error("error connecting " + err.stack);
         return;
     }
     console.log("connected as id " + connection.threadId);
- });
+ }); */
 //export the module
 
 connection.connect();
